4f4356c3213249bfd6919f16e9c8c853592655f4
dhiraj19999 <yogeshsamse682@gmail.com>
2024-03-16 10:57:49 +0530

    postRouted-added

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import cookieParser from "cookie-parser";
 import connectDB from "./db/connectDB.js";
-
+import postRoutes from "./routes/postRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,9 +14,9 @@ connectDB();
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
-
+// rouets
 app.use("/api/users", userRoutes);
-
+app.use("/api/posts", postRoutes);
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
 );
